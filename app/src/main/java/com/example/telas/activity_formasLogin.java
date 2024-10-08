37f9c9a353a40651cb9199f2d1e1ba793c52ec94
LeandroSouzaFontes <leandrosff7@gmail.com>
2024-10-08 13:48:20 -0300

    Google sign

--- app/src/main/java/com/example/telas/activity_formasLogin.java
+++ app/src/main/java/com/example/telas/activity_formasLogin.java
@@ -1,15 +1,31 @@
 package com.example.telas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class activity_formasLogin extends AppCompatActivity {
 
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    Button google_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +36,42 @@ public class activity_formasLogin extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        google_btn = findViewById(R.id.entrarGoogle);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+
+
+        google_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+    void signIn(){
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if(requestCode == 1000){
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    task.getResult(ApiException.class);
+                    nextActivity();
+                } catch (ApiException e) {
+                    Toast.makeText(this, "Algo deu errado", Toast.LENGTH_SHORT).show();
+                }
+            }
+    }
+
+    void nextActivity(){
+        finish();
+        Intent intent = new Intent(activity_formasLogin.this, activity_login.class);
+        startActivity(intent);
     }
 }
